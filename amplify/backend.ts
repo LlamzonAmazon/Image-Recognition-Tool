fec882ef62f524ea149ea7e9c693ffbef925475c
Thomas Llamzon <tllamzon@uwo.ca>
2025-01-12 18:41:01 -0500

    Update backend.ts

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -4,19 +4,9 @@ import { data } from './data/resource';
 
 import { storage } from './storage/resource';
 
-
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
- 
-defineBackend({
-  auth,
-  data,
-});
 */
-
-/**
- * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
- */
 defineBackend({
   auth,
   data,
