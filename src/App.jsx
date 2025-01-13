f788dc08db05cf5ee3b00e7fe9160cee2a7ee11f
Thomas Llamzon <tllamzon@uwo.ca>
2025-01-13 15:40:45 -0500

    logos back on there

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,14 @@ export default function App() {
     <Authenticator>
       {({ signOut }) => (
         <Flex direction="column" justifyContent="center" alignItems="center">
+          <div>
+            <a href="https://vite.dev" target="_blank">
+              <img src={viteLogo} className="logo" alt="Vite logo" />
+            </a>
+            <a href="https://react.dev" target="_blank">
+              <img src={reactLogo} className="logo react" alt="React logo" />
+            </a>
+          </div>
           <h1>Vite + React Project by Thomas</h1>
           <div className="card">
             <button onClick={() => setCount(count + 1)}>
